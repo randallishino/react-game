9968fb28333660f17acc4321adfd24eebbb943f4
randall ishino <rishino24@gmail.com>
2018-04-16 15:45:58 -0700

    working on app

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import FriendCard from "./components/FriendCard";
+import Wrapper from "./components/Wrapper";
+import Title from "./components/Title";
+import Nav from "./components/Navbar";
+import friends from "./friends.json";
+
+
+
+let counter=0;
+let imageArray=[];
+let highScore=0;
+let headline="Test your knowledge of memory. Click a gif but not the same one twice!";
 
 class App extends Component {
   render() {
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Navbar } from 'react-bootstrap';
 
-const Navbar = props => (
+const Nav = props => (
 
 
     <Navbar>
